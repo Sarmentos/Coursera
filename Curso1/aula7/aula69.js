6362b8b6556cc68cca888f9dadb124f852f4e0ce
Lucas Silva Sarmento <lucassilvasarmentos3@gmail.com>
2026-02-10 11:46:07 -0300

    fix: aula de forEach

--- Curso1/aula7/aula69.js
+++ Curso1/aula7/aula69.js
@@ -1,3 +1,12 @@
 const a1 = [1, 2, 3, 4, 5,6,6,7,8,9];
 
+// For clássico
+for (let valor of a1) {
+    console.log(valor);
+}
 
+// ForEach
+let total = 0;
+a1.forEach((valor) => total += valor);
+console.log(total);
+a1.forEach((valor) => console.log(valor));
